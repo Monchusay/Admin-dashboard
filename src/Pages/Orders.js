4bda427c031c5ab653fc66cfaab5344f315c22e1
Monchusay <armydolgov@mail.ru>
2022-07-10 06:47:44 +0300

    OrdersPage

--- src/Pages/Orders.js
+++ src/Pages/Orders.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../Data/dummy";
+import { Header } from "../Components";
 
 const Orders = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="m-2 md:m-10 p2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
